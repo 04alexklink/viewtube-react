5a52e80a318c156f5a1510e3aaa68fba43a45936
04alexklink <alex_klink123@hotmail.co.uk>
2021-05-28 15:31:52 +0100

    Search bar css styling

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -8,13 +8,13 @@ const SearchBar = () => {
     e.preventDefault();
     console.log(searchText)
   }
+
   return (
     <div className="search-bar">
       <form className="form-control" onSubmit={submitSearch}>
         <label>Search</label>
         <input onChange={(e) => setSearchText(e.target.value)} type="text" value={searchText} />
-      </form>
-      
+      </form> 
     </div>
   )
 }
