a2679dd50966bdb46f96f65a8d05085148b42ab1
04alexklink <alex_klink123@hotmail.co.uk>
2021-05-28 15:13:26 +0100

    Search bar form onSubmit created. Not hooked up to youtube API yet. Just console.logs.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,9 +4,13 @@ const SearchBar = () => {
 
   const [searchText, setSearchText] = useState("");
 
+  const submitSearch = (e) => {
+    e.preventDefault();
+    console.log(searchText)
+  }
   return (
     <div className="search-bar">
-      <form className="form-control">
+      <form className="form-control" onSubmit={submitSearch}>
         <label>Search</label>
         <input onChange={(e) => setSearchText(e.target.value)} type="text" value={searchText} />
       </form>
