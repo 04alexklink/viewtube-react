1151a6a695d700216944951bbe04a6bcd6bd0e91
04alexklink <alex_klink123@hotmail.co.uk>
2021-05-29 12:25:40 +0100

    getVideos function created to make axios get request to youtube API and returns 6 relevant videos to the search term inputted into the function

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import './App.css';
 import Header from './components/Header';
 import SearchBar from './components/SearchBar';
+import axios from 'axios';
+import {useState} from 'react';
 
 function App() {
+
+  const [videos, setVideos] = useState([])
+  
+  const getVideos = async (searchTerm) => {
+    const baseUrl = "https://www.googleapis.com/youtube/v3";
+    const videos = await axios.get(baseUrl + "/search", {
+      params: {
+        q: searchTerm,
+        part: "snippet",
+        maxResults: 6,
+        type: "video",
+        key: process.env.REACT_APP_YOUTUBE_API_KEY
+      }
+    })
+    return videos;
+  }
+
+  getVideos("cats")
   return (
     <div className="container">
     <Header title="Viewtube" ></Header>
