ed3a03ff8cfb405b2f7c062abfafb323370b93b3
04alexklink <alex_klink123@hotmail.co.uk>
2021-05-30 19:14:20 +0100

    Current Video description styled

--- src/components/CurrentVideo.js
+++ src/components/CurrentVideo.js
@@ -3,10 +3,10 @@ const CurrentVideo = () => {
   return (
     <div className="current-video">
       <div className="video-wrapper">
-      <iframe width="100%" height="315" src="https://www.youtube.com/embed/0L8cQ9nRtuE" title="YouTube video player" frameBorder="0"></iframe>
+      <iframe src="https://www.youtube.com/embed/0L8cQ9nRtuE" title="YouTube video player" frameBorder="0"></iframe>
       </div>
       <div className="current-video-info">
-       <h1>Video.....</h1>
+       <h4>Video.....</h4>
        <p>Hardcoded video info is here........</p>
       </div>
     </div>
