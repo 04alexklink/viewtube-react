78a0dd1d14bf43b3d07f40738cb6799088bccbf4
04alexklink <alex_klink123@hotmail.co.uk>
2021-06-03 12:07:48 +0100

    Testing: App component/integration test: search bar with input text area is present on the screen

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+describe('when app first loads', () => {
+  test('search bar is visible to the user', () => {
+    render(<App />);
+    // const linkElement = screen.getByText(/learn react/i);
+    // expect(linkElement).toBeInTheDocument();
+    screen.debug();
+    expect(screen.getByRole('textbox')).toBeInTheDocument()
+    expect(screen.getByText('Please enter a search above')).toBeInTheDocument()
+  });
+  // video container and video list items are not shown on the screen
+})
